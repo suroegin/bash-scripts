ab1ec117f746a76c9acebf242d0f09b3ebc6cd7d
Ivan Suroegin <ivan.suroegin@gmail.com>
2017-02-22 15:22:04 +0300

    Update opencv.sh

--- opencv.sh
+++ opencv.sh
@@ -4,32 +4,5 @@ sudo apt-get install -y libatlas-base-dev gfortran
 sudo apt-get install -y libxvidcore-dev libx264-dev
 sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
-sudo pip install -y virtualenv virtualenvwrapper
-
-cd ~
-wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.2.0.zip
-unzip opencv_contrib.zip
-
-export WORKON_HOME=$HOME/.virtualenvs
-source /usr/local/bin/virtualenvwrapper.sh
-
-echo -e "\n# virtualenv and virtualenvwrapper" >> ~/.bashrc
-echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc
-echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
-source ~/.bashrc
-
-mkvirtualenv cv -p python3
-workon cv
-pip install numpy
-
-cd ~/opencv-3.1.0/
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
-    -D CMAKE_INSTALL_PREFIX=/usr/local \
-    -D INSTALL_PYTHON_EXAMPLES=ON \
-    -D INSTALL_C_EXAMPLES=OFF \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.2.0/modules \
-    -D PYTHON_EXECUTABLE=~/.virtualenvs/cv/bin/python \
-    -D BUILD_EXAMPLES=ON ..
+sudo apt-get install -y python-dev python-numpy libtbb2
+pip3 install opencv-python
