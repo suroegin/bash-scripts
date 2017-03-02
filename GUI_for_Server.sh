50d56644f1f05710be03d34b222e1c831a9fb429
Ivan Suroegin <ivan.suroegin@gmail.com>
2017-03-02 10:34:17 +0300

    Update GUI_for_Server.sh

--- GUI_for_Server.sh
+++ GUI_for_Server.sh
@@ -69,7 +69,7 @@ case "$setnumber" in
                 apt install xorg
                 apt install --no-install-recommends ubuntu-mate-core
                 ;;
-        
+        esac
         ;;
     5) echo "Your choice is 5. A full lightweight desktop environment without minding the recommendations."
         echo ""
@@ -91,8 +91,7 @@ case "$setnumber" in
             3) echo ""
                 apt install ubuntu-mate-core
                 ;;
-
-    
+        esac
         ;;
     6) echo "Your choice is 6. A full desktop with all the extras."
         echo ""
@@ -126,6 +125,7 @@ case "$setnumber" in
             6) echo ""
                 apt install kubuntu-desktop
                 ;;
+        esac
         ;;
     tips|TIPS) echo ""
         echo "TIP1: The --no-install-recommended options applies to all dependencies packages recursively so I first install xorg package to make sure all graphic drivers and other packages are installed and so that my system is portable even if I change motherboard or gpu. Some people install only components of xorg but I've never been able to create a usable system this way."
@@ -134,7 +134,6 @@ case "$setnumber" in
         echo "TIP4: At options 2 to 6 if you wanna stop lightdm autostarting then run the command sudo systemctl disable lightdm and you can start it whenever you want with sudo systemctl start lightdm. To re-enable it run sudo systemctl enable lightdm and check it with systemctl is-enabled lightdm (sometimes you can't re-enable it and the is-enabled commands has output static so run sudo apt install -reinstall lightdm to fix it)"
         echo "TIP5: There is also another option (which I left out on purpose). You can install the specific desktop environment metapackage like lxde, xfce4, mate-desktop-environment, plasma-desktop, unity, gnome. However, you will need more packages that just xorg in most cases and these packages or metapackages might install packages that are not longer preferred by any Ubuntu flavor. For example lxde installs wicd as recommendation when all flavors (including lubuntu) use network-manager and network-manager-gnome nowadays. To see differences between packages you can search here: http://packages.ubuntu.com/"
         ;;
-        
     *) echo "Nothing typed, cancelled..."
         exit 0
         ;;
