a02278264db6cae2c7ac173ecefd703ea877f9c3
Ivan Suroegin <ivan.suroegin@gmail.com>
2017-03-02 10:11:34 +0300

    Update GUI_for_Server.sh

--- GUI_for_Server.sh
+++ GUI_for_Server.sh
@@ -10,6 +10,9 @@ echo "4. A full lightweight desktop environment"
 echo "5. A full lightweight desktop environment without minding the recommendations"
 echo "6. A full desktop with all the extras"
 echo ""
+echo "You can read read about explanation about all sets, just type H and number, i.e. H1 - will show you help about Ultra minimal GUI."
+echo "So, just type H (without number) for read tips."
+echo ""
 echo -n "Please choose GUI set:"
 
 read setnumber
@@ -18,13 +21,13 @@ case "$setnumber" in
         apt install xorg
         apt install --no-install-recommends openbox
         ;;
-    2) echo "Your choice is 2. Minimal GUI with display manager (xorg, openbox and lightdm)"
+    2) echo "Your choice is 2. Minimal GUI with display manager (xorg, openbox and lightdm)."
         apt install xorg
         apt install --no-install-recommends lightdm-gtk-greeter
         apt install --no-install-recommends lightdm
         apt install --no-install-recommends openbox
         ;;
-    3) echo "Your choice is 3. A more functional minimal desktop environment"
+    3) echo "Your choice is 3. A more functional minimal desktop environment."
         apt install xorg
         apt install –no-install-recommends lightdm-gtk-greeter
         apt install –no-install-recommends lightdm
@@ -35,82 +38,113 @@ case "$setnumber" in
         apt install –no-install-recommends lxsession-logout
         apt install –no-install-recommends gvfs-backends
         ;;
-    4) echo "Your choice is 4. A full lightweight desktop environment"
+    4) echo "Your choice is 4. A full lightweight desktop environment (--no-install-recommends)."
         echo ""
-        echo "1. "
-        echo "2. "
-        echo "3. "
+        echo "1. lubuntu-core (--no-install-recommends)"
+        echo "2. xubuntu-core (--no-install-recommends)"
+        echo "3. ubuntu-mate-core (--no-install-recommends)"
         echo ""
-        echo -n "Please choose desktop environment"
+        echo -n "Please choose desktop environment:"
         read variant
         case "$variant" in
             1) echo ""
-                ..
+                apt install xorg
+                apt install --no-install-recommends lubuntu-core
                 ;;
             2) echo ""
-                ..
+                apt install xorg
+                apt install --no-install-recommends xubuntu-core
                 ;;
             3) echo ""
-                ..
+                apt install xorg
+                apt install --no-install-recommends ubuntu-mate-core
                 ;;
         
         ;;
-    5) echo "Your choice is 5. A full lightweight desktop environment without minding the recommendations"
+    5) echo "Your choice is 5. A full lightweight desktop environment without minding the recommendations."
         echo ""
-        echo "1. "
-        echo "2. "
-        echo "3. "
+        echo "1. lubuntu-core"
+        echo "2. xubuntu-core"
+        echo "3. ubuntu-mate-core"
         echo ""
-        echo -n "Please choose desktop environment"
+        echo -n "Please choose desktop environment:"
         read variant
         case "$variant" in
             1) echo ""
-                ..
+                apt install lubuntu-core
                 ;;
             2) echo ""
-                ..
+                apt install xubuntu-core
                 ;;
             3) echo ""
-                ..
+                apt install ubuntu-mate-core
                 ;;
 
     
         ;;
-    6) echo "Your choice is 6. A full desktop with all the extras"
+    6) echo "Your choice is 6. A full desktop with all the extras."
         echo ""
-        echo "1. "
-        echo "2. "
-        echo "3. "
-        echo "4. "
-        echo "5. "
-        echo "6. "
+        echo "1. lubuntu-desktop"
+        echo "2. xubuntu-desktop"
+        echo "3. ubuntu-mate-desktop"
+        echo "4. ubuntu-gnome-desktop"
+        echo "5. ubuntu-desktop"
+        echo "6. kubuntu-desktop"
         echo ""
-        echo -n "Please choose desktop environment"
+        echo -n "Please choose desktop environment:"
         read variant
         case "$variant" in
             1) echo ""
-                ..
+                apt install lubuntu-desktop
                 ;;
             2) echo ""
-                ..
+                apt install xubuntu-desktop
                 ;;
             3) echo ""
-                ..
+                apt install ubuntu-mate-desktop
                 ;;
             4) echo ""
-                ..
+                apt install ubuntu-gnome-desktop
                 ;;
             5) echo ""
-                ..
+                apt install ubuntu-desktop
                 ;;
             6) echo ""
-                ..
+                apt install kubuntu-desktop
                 ;;
-    
         ;;
-    x|X) echo "Cancelled..."
-        exit 0
+    h|H) echo ""
+        echo "TIP1: The --no-install-recommended options applies to all dependencies packages recursively so I first install xorg package to make sure all graphic drivers and other packages are installed and so that my system is portable even if I change motherboard or gpu. Some people install only components of xorg but I've never been able to create a usable system this way."
+        echo "TIP2: If an option you choose installs network-manager and network-manager-gnome then better use it to configure your network and delete everything at /etc/network/interfaces file (except the lo interface) in order to avoid conflicts."
+        echo "TIP3: If you need remote desktop via x11vnc then choose option 2 to 6 (I think you also need to add option -auth guess and -loopso that vnc works before you login and after you logout)"
+        echo "TIP4: At options 2 to 6 if you wanna stop lightdm autostarting then run the command sudo systemctl disable lightdm and you can start it whenever you want with sudo systemctl start lightdm. To re-enable it run sudo systemctl enable lightdm and check it with systemctl is-enabled lightdm (sometimes you can't re-enable it and the is-enabled commands has output static so run sudo apt install -reinstall lightdm to fix it)"
+        echo "TIP5: There is also another option (which I left out on purpose). You can install the specific desktop environment metapackage like lxde, xfce4, mate-desktop-environment, plasma-desktop, unity, gnome. However, you will need more packages that just xorg in most cases and these packages or metapackages might install packages that are not longer preferred by any Ubuntu flavor. For example lxde installs wicd as recommendation when all flavors (including lubuntu) use network-manager and network-manager-gnome nowadays. To see differences between packages you can search here: http://packages.ubuntu.com/"
         ;;
+
+    h1|H1) echo ""
+        echo ""
+        ;;
+
+    h2|H2) echo ""
+        echo ""
+        ;;
+        
+    h3|H3) echo ""
+        echo ""
+        ;;
+        
+    h4|H4) echo ""
+        echo ""
+        ;;
+        
+    h5|H5) echo ""
+        echo ""
+        ;;
+        
+    h6|H6) echo ""
+        echo ""
+        ;;
+        
     *) echo "Nothing typed, cancelled..."
         exit 0
         ;;
