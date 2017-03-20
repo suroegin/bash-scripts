e2a5e17bdaa4974471ad729f9089cb9a5f0dfb29
Ivan Suroegin <ivan.suroegin@gmail.com>
2017-03-20 13:27:51 +0300

    Update add_site_to_nginx.sh

--- add_site_to_nginx.sh
+++ add_site_to_nginx.sh
@@ -10,23 +10,28 @@ echo "Folders which $USERNAME have in /projects"
 /bin/ls -alF /home/$USERNAME/projects
 
 echo "Folder name - is a folder of a types of projects. Types:"
+echo ""
 echo "    With Python: pyWEB | pyBOTS | pySCRIPTS | pyPARSER | pyNOTEBOOKS"
 echo "    With R: rSCRIPTS | rPARSER | rNOTEBOOKS"
 echo "    With Javascript: jsWEB | jsBOTS | jsSCRIPTS | jsPARSER"
 echo "    Simple static sites: htmlSITES | mkSITES"
 echo "    Temporary small projects or scripts: TEMPORARY" 
-
-echo -n "\nFolder name?"
+echo ""
+echo -n "Folder name? : "
 read FOLDER_NAME
-echo -n "\nEnter name of new project (without spaces and not standard symbols, please)"
+echo -n "Enter name of new project (without spaces and not standard symbols, please) : "
 read PROJECT_NAME
 
 /bin/mkdir -p /home/$USERNAME/projects/$FOLDER_NAME/$PROJECT_NAME
-echo "\n/home/$USER/projects/$FOLDER_NAME/$PROJECT_NAME directory is created."
+echo ""
+echo "/home/$USER/projects/$FOLDER_NAME/$PROJECT_NAME directory is created."
 
-sudo /bin/cp /etc/nginx/sites-available/default /etc/nginx/sites-available/$FOLDER_NAME-$PROJECT_NAME
+chown -R $USER:$USER /home/$USERNAME/projects/$FOLDER_NAME/$PROJECT_NAME
+chmod -R 755 /home/$USERNAME/projects/
 
-sudo /bin/cat <<FOO > /etc/nginx/sites-available/$FOLDER_NAME-$PROJECT_NAME
+/bin/cp /etc/nginx/sites-available/default /etc/nginx/sites-available/$FOLDER_NAME-$PROJECT_NAME
+
+/bin/cat <<FOO > /etc/nginx/sites-available/$FOLDER_NAME-$PROJECT_NAME
 server {
     listen 80;
     server_name $IP_ADDRESS;
@@ -38,7 +43,7 @@ server {
 FOO
 
 
-sudo /bin/ln -s /etc/nginx/sites-available/$FOLDER_NAME-$PROJECT_NAME /etc/nginx/sites-enabled/$FOLDER_NAME-$PROJECT_NAME
-sudo /usr/sbin/service nginx restart
+/bin/ln -s /etc/nginx/sites-available/$FOLDER_NAME-$PROJECT_NAME /etc/nginx/sites-enabled/$FOLDER_NAME-$PROJECT_NAME
+/usr/sbin/service nginx restart
 
 echo "Now you can upload project to: /home/$USERNAME/projects/$FOLDER_NAME/$PROJECT_NAME"
