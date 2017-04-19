a55f51e4aafa5e391683fffc30f22c0b9276d75f
Ivan Suroegin <ivan.suroegin@gmail.com>
2017-04-19 13:31:22 +0300

    Create install-jupyter.sh

---
+++ install-jupyter.sh
@@ -0,0 +1,24 @@
+apt install python3 python3-pip
+pip3 install virtualenv
+
+cd ~
+mkdir jupyter
+mkdir .virtualenvs
+
+virtualenv ~/.virtualenvs/jupyter
+source ~/.virtualenvs/jupyter/bin/activate
+
+pip install ipython
+pip install jupyter
+
+jupyter notebook --generate-config
+
+
+/bin/cat <<FOO > ~/.jupyter/jupyter_notebook_config.py
+c.NotebookApp.ip = '*'
+c.NotebookApp.password = 'sha1:13a2e1b876ec:f44d1c967eb3651ad93036e88bb1a7b2e60054cb'
+c.NotebookApp.open_browser = False
+c.NotebookApp.port = 5235
+FOO
+
+echo "\nReady! Just type: jupyter notebook and open link in the browser"
