16e2c560a60681804091cee56d12a4d645e18e3f
Ivan Suroegin <ivan.suroegin@gmail.com>
2017-09-26 17:19:44 +0300

    Update fix-vscode.sh

--- fix-vscode.sh
+++ fix-vscode.sh
@@ -1,2 +1,5 @@
 sudo cp /usr/lib/x86_64-linux-gnu/libxcb.so.1.1.0 /usr/share/code/libxcb.so.1
 sudo sed -i 's/BIG-REQUESTS/_IG-REQUESTS/' /usr/share/code/libxcb.so.1
+
+
+# wget -O - https://raw.githubusercontent.com/suroegin/bash/master/fix-vscode.sh | bash
