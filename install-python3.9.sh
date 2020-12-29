5facf5e759f52ad768c60d81cb13f812a076cc12
Ivan Suroegin <ivan.suroegin@gmail.com>
2020-12-29 22:56:13 +0300

    Update and rename install-python3.7.sh to install-python3.9.sh

--- install-python3.9.sh
+++ install-python3.9.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y build-essential libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev tk-dev python-dev python-setuptools python-pip python-smbus libc6-dev openssl libffi-dev
 
-# Install Python 3.7.0
+# Install Python 3.9.0
 cd /opt/
 sudo wget https://www.python.org/ftp/python/3.9.0/Python-3.9.0.tar.xz
 sudo tar xzf Python-3.9.0.tar.xz
