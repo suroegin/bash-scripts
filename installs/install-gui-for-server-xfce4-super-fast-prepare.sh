d0fba0e8d3ee7f38bc5f94b991189f45d53bdb43
Ivan Suroegin <ivan.suroegin@gmail.com>
2021-11-17 12:52:26 +0300

    Update install-gui-for-server-xfce4-super-fast-prepare.sh

--- installs/install-gui-for-server-xfce4-super-fast-prepare.sh
+++ installs/install-gui-for-server-xfce4-super-fast-prepare.sh
@@ -5,3 +5,13 @@ apt-get install -y xrdp && \
 apt-get install -y xfce4 xfce4-terminal && \
 echo "xfce4-session" > ~/.xsession && \
 systemctl restart xrdp
+
+
+# Install Firefox
+apt install firefox
+
+# Install Google Chrome (with fix)
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
+apt --fix-broken install -y
+dpkg -i google-chrome-stable_current_amd64.deb
